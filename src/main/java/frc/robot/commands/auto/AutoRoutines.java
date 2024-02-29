7ba282a046eed91d9d4239068ac8ab6ffaa5ceec
Eric Macy <illinihiker@outlook.com>
2024-02-29 18:40:13 -0500

    Auton Debug

--- src/main/java/frc/robot/commands/auto/AutoRoutines.java
+++ src/main/java/frc/robot/commands/auto/AutoRoutines.java
@@ -4,6 +4,7 @@ import com.pathplanner.lib.commands.PathPlannerAuto;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.SwerveDrive;
@@ -16,23 +17,18 @@ public class AutoRoutines {
         public static Command ShootNoteAndLeave(ShooterWheels shooter, IntakeWheels intake, IntakeArm intakeArm,
                         SwerveDrive swerve) {
                 return Commands.sequence(
-                                new StartEndCommand(() -> intakeArm.setIntakePosition(IntakeArmConstants.kShootSpeaker),
-                                                () -> intakeArm.setIntakePosition(IntakeArmConstants.kShootSpeaker),
-                                                intakeArm),
-                                new StartEndCommand(() -> shooter.runWheels(),
-                                                null,
-                                                shooter),
+                                new RunCommand(() -> intakeArm.setIntakePosition(IntakeArmConstants.kShootSpeaker),
+                                                intakeArm).withTimeout(1),
+                                new RunCommand(() -> shooter.runWheels(), shooter).withTimeout(1),
                                 new WaitCommand(2),
                                 new StartEndCommand(() -> intake.intake(1),
                                                 () -> intake.intake(0),
                                                 intake)
                                                 .withTimeout(1),
-                                new StartEndCommand(() -> shooter.runWheels(),
-                                                null,
-                                                shooter),
+                                new RunCommand(() -> shooter.runWheels(), shooter).withTimeout(1),
                                 new StartEndCommand(() -> intakeArm.setIntakePosition(IntakeArmConstants.kZeroPosition),
                                                 () -> intakeArm.setIntakePosition(IntakeArmConstants.kZeroPosition),
-                                                intakeArm),
-                                new PathPlannerAuto("Test Auto"));
+                                                intakeArm).withTimeout(1),
+                                new PathPlannerAuto("Test Auto").withTimeout(5));
         }
 }
